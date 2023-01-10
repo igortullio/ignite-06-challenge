65b02cb8a6697257cfeedbe4eaddcc0ce9372fd3
Igor Tullio <igortullio@hotmail.com>
2023-01-10 13:24:07 -0300

    Add next auth with google provider and custom adapter

---
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -0,0 +1,58 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
+import { PrismaAdapter } from '../../../lib/next-auth/prisma-adapter'
+
+const userInfoEmailScope = 'https://www.googleapis.com/auth/userinfo.email'
+const userInfoProfileScope = 'https://www.googleapis.com/auth/userinfo.profile'
+const calendarScope = 'https://www.googleapis.com/auth/calendar'
+
+export function buildNextAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
+
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        authorization: {
+          params: {
+            scope: `${userInfoEmailScope} ${userInfoProfileScope} ${calendarScope}`,
+          },
+        },
+        profile(profile: GoogleProfile) {
+          return {
+            id: profile.sub,
+            name: profile.name,
+            username: '',
+            email: profile.email,
+            avatar_url: profile.picture,
+          }
+        },
+      }),
+    ],
+
+    callbacks: {
+      async signIn({ account }) {
+        if (!account?.scope?.includes(calendarScope)) {
+          return '/register/connect-calendar?error=permissions'
+        }
+        return true
+      },
+
+      async session({ session, user }) {
+        return {
+          ...session,
+          user,
+        }
+      },
+    },
+  }
+}
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res))
+}
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -49,6 +49,8 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
+
+      await router.push('/register/connect-calendar')
     } catch (err) {
       console.log(err)
     }
